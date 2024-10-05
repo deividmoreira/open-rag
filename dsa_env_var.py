ab4bc76a51e37cc01491ad8c058805caa5c3a199
Deivid Moreira <moreira.deivid@gmail.com>
2024-10-05 16:07:45 -0300

    primeira versão do projeto

---
+++ dsa_env_var.py
@@ -0,0 +1,9 @@
+# Projeto 9 - IA Generativa e RAG Para App de Sistema Inteligente de Busca em Documentos - Backend e API
+# Módulo da API Para o LLM
+
+# Import
+import os
+
+# Nvidia API
+nvidia_key = "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"
+
